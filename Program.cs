baa9217cb5db464917af54260ec12c1b71a78a96
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2023-11-16 12:21:34 -0500

    Add repro

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+					
+public class Program
+{
+    private const string TypeSegmentPattern = "[a-z0-9][a-z0-9-.]*";
+    private const string VersionPattern = "[a-z0-9][a-z0-9-]+";
+
+    private const RegexOptions PatternRegexOptions = RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.CultureInvariant;
+    private static readonly Regex ResourceTypePattern = new Regex(@$"^(?<type>{TypeSegmentPattern})(/(?<type>{TypeSegmentPattern}))*(@(?<version>{VersionPattern}))?$", PatternRegexOptions);
+	
+    public static void Main()
+    {
+        ResourceTypePattern.Match("Microsoft.Compute/virtualMachines@2022-01-01");
+        Console.WriteLine("Success!");
+    }
+}
